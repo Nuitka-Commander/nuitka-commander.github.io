f37a3c8faa986625dd98062f6b3392291ff29d1c
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-09-15 00:06:44 +0800

    毛玻璃！

--- scripts/menu_setting.js
+++ scripts/menu_setting.js
@@ -1,16 +1,14 @@
 function openMenu() {
-    console.log("open")
     let open_menu_button = document.querySelector(".open_menu_button");
     open_menu_button.style.visibility = "hidden";
     open_menu_button.style.opacity = "0";
-    document.getElementById("menu_sidebar").style.width = Math.max(window.innerWidth * 0.15, 150) + "px";
-
-
+    let menu_sidebar = document.getElementById("menu_sidebar");
+    menu_sidebar.style.width = Math.max(window.innerWidth * 0.15, 150) + "px";
 }
 
 function closeMenu() {
-      console.log("close")
-    document.getElementById("menu_sidebar").style.width = "0";
+    let menu_sidebar = document.getElementById("menu_sidebar");
+    menu_sidebar.style.width = "0";
     let open_menu_button = document.querySelector(".open_menu_button");
     open_menu_button.style.visibility = "visible";
     open_menu_button.style.opacity = "1";
