cf85dfa07a7e10cf25631237698adfd0cef5081e
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-09-19 23:36:56 +0800

    加载语言函数完成 准备测试

--- scripts/generic_functions.js
+++ scripts/generic_functions.js
@@ -6,9 +6,23 @@ const current_language = {
     "start": "启动"
 };
 
-// 刷新body来更新语言
-function t(key) {
-    return current_language[key];
+// todo 刷新body来更新语言
+//加载语言到字典中
+function load_language() {
+    fetch('./src/translation/supported_languages.json')
+        .then(response => response.json())
+        .then(data => {
+            let a = data;
+            console.log(a);
+        })
+        .catch(error => {
+            console.error('An error occurred in loading the language:', error);
+            alert('An error occurred while loading the language! Please provide us with an issue. \nError:' + error);
+        });
 }
 
-console.log(`${t("Genshin")}?${t("start")}!`);
+//翻译函数 输入key 从当前语言字典中获取key对应的当前语言字符串
+function t(key) {
+
+    return current_language[key];
+}
