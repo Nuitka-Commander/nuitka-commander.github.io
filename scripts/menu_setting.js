e943d00cc6cd2638537625a578724622b2ef73ba
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-09-27 00:06:50 +0800

    完成了按钮 等待颜色变量

--- scripts/menu_setting.js
+++ scripts/menu_setting.js
@@ -1,6 +1,7 @@
 // *********************
 // 处理在菜单操作处理的逻辑
 // *********************
+
 function openMenu() {
     let open_menu_button = document.querySelector(".open_menu_button");
     open_menu_button.style.visibility = "hidden";
@@ -8,6 +9,7 @@ function openMenu() {
     let menu_sidebar = document.getElementById("menu_sidebar");
     menu_sidebar.style.width = Math.max(window.innerWidth * 0.15, 150) + "px";
 }
+
 
 function closeMenu() {
     let menu_sidebar = document.getElementById("menu_sidebar");
@@ -17,8 +19,23 @@ function closeMenu() {
     open_menu_button.style.opacity = "1";
 }
 
-// css darkmode适配：     document.body.classList.remove('dark_mode')    document.body.classList.add('dark_mode')
-//打开theme下拉框
-function show_theme_dropdown() {
-    document.getElementById("theme_change").classList.toggle("show");
+// css dark mode适配：     document.body.classList.remove('dark_mode')    document.body.classList.add('dark_mode')
+
+//打开下拉栏
+function myFunction() {
+    document.getElementById("theme_change-dropdown_content").classList.toggle("show");
 }
+
+// 如果用户在下拉菜单之外点击，则关闭下拉菜单
+window.onclick = function (event) {
+    if (!event.target.matches(".change_theme_btn")) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+};
