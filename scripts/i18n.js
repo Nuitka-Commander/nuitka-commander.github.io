9abd172fce9df5eb75aa6352240b55502299195b
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-09-30 15:35:48 +0800

    修改一些函数位置~

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -1,38 +1,18 @@
 // *********************
-// 处理翻译及加载逻辑
+// 处理翻译相关的脚本
 // *********************
 
 
-
-function start_loading() {
-    document.querySelectorAll('.loading_page span').forEach(span => {
-        span.style.animationPlayState = "running"; // 恢复动画
+//加载"可用语言"对象
+fetch("./src/translation/supported_languages.json")
+    .then(response => response.json())
+    .then(data => {
+        supported_languages = data;
+    })
+    .catch(error => {
+        console.error("An error occurred in loading the language:", error);
+        alert("An error occurred while loading the language! Please provide us with an issue. \nError:" + error);
     });
-    document.querySelector('.loading_page').style.display = "flex";
-
-}
-
-function stop_loading() {
-    document.querySelectorAll('.loading_page span').forEach(span => {
-        span.style.animationPlayState = "paused"; // 暂停动画
-    });
-    document.querySelector('.loading_page').style.display = "none";
-}
-
-// ===========================================
-
-//加载语言到字典中
-function load_language() {
-    fetch('./src/translation/supported_languages.json')
-        .then(response => response.json())
-        .then(data => {
-            current_language = data;
-        })
-        .catch(error => {
-            console.error('An error occurred in loading the language:', error);
-            alert('An error occurred while loading the language! Please provide us with an issue. \nError:' + error);
-        });
-}
 
 //根据键从翻译字典中获得翻译
 function get_translation(key) {
@@ -42,9 +22,9 @@ function get_translation(key) {
 
 //根据tKey对整个页面的元素进行重载翻译文本
 function reload_all_translation() {
-    let elements = document.querySelectorAll('[tKey]');
+    let elements = document.querySelectorAll("[tKey]");
     elements.forEach(element => {   //遍历所有有有tKey属性的标签
-        element.textContent = get_translation(element.getAttribute('tKey'));
+        element.textContent = get_translation(element.getAttribute("tKey"));
     });
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,18 @@
 let current_language; //存储翻译的json
+let supported_languages; //存储支持的语言对象
 let localStorage_available;//localStorage是否可用
+let options = {
+    "theme": "auto", "language": "en_us",
+};//todo update监听
+
 // ===========================================
+
 //检测localStorage是否可用并检测是否已满(如果不可用则返回false)
 function check_localStorage() {
     try {
-        let x = "__storage_test__";
+        const x = "__storage_test__";
         localStorage.setItem(x, x);
+        localStorage.getItem(x);
         localStorage.removeItem(x);
         return true;
     } catch (e) {
@@ -13,6 +20,41 @@ function check_localStorage() {
         alert("Unable to store data locally! This will not record your choices. Please try changing your browser or starting localStorage.\nError: " + e);
         return false;
     }
+
 }
 
 localStorage_available = check_localStorage();
+//尝试加载localStorage中的数据到本地字典中
+if (localStorage_available) {
+    try {
+        let options_in_localStorage = JSON.parse(localStorage.getItem("options"));
+        for (let key in options) {
+            if (options_in_localStorage.hasOwnProperty(key)) {
+                options[key] = options_in_localStorage[key];
+            }
+        }
+    } catch (e) {
+        console.warn("Unable to load data from localStorage! \nError: " + e);
+        alert("Unable to load data from localStorage! \nError: " + e);
+    }
+}
+
+
+// ===========================================
+
+//打开加载页面
+function start_loading() {
+    document.querySelectorAll(".loading_page span").forEach(span => {
+        span.style.animationPlayState = "running"; // 恢复动画
+    });
+    document.querySelector(".loading_page").style.display = "flex";
+
+}
+
+//关闭加载页面
+function stop_loading() {
+    document.querySelectorAll(".loading_page span").forEach(span => {
+        span.style.animationPlayState = "paused"; // 暂停动画
+    });
+    document.querySelector(".loading_page").style.display = "none";
+}
