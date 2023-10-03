56a0f19721762667f445c246df03eff80cf0d931
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-10-04 00:35:43 +0800

    主题切换css部分完成! 只差显示的内容和一个动画~

--- scripts/theme.js
+++ scripts/theme.js
@@ -3,20 +3,26 @@
 // *********************
 
 
-// css dark mode适配：     document.body.classList.remove('dark_mode')    document.body.classList.add('dark_mode')
-
 function change_theme(theme) {
-    //todo 更改 theme
-    //todo 设置当前主题对于用户的选项
-
+    //设置当前选择的主题按钮
+    const buttons = document.getElementsByClassName("change_theme_btn");
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        // 如果当前按钮对应的主题与传入的主题相同，则为该按钮添加selected_theme类
+        if (button.id === theme + '_mode_btn') {
+            button.style.border="1px solid";
+            button.style.borderColor = "var(--hover-color)";
+        } else { // 否则，删除该按钮的selected_theme类
+            button.style.border="none";
+        }
+    }
+    //设置主题
     options["theme"] = theme;
     switch (theme) {
         case "light":
-            console.log("change_theme: light");
             document.body.classList.remove("dark_mode");
             break;
         case "dark":
-            console.log("change_theme: dark");
             document.body.classList.add("dark_mode");
             break;
         case "auto":
@@ -29,7 +35,6 @@ function change_theme(theme) {
 //根据用户设置的主题设置页面主题
 function auto_set_theme() {
     if (options["theme"] === "auto") {
-        console.log("auto")
         if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
             document.body.classList.add("dark_mode");
         } else {
@@ -37,5 +42,6 @@ function auto_set_theme() {
         }
     }
 }
+
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", auto_set_theme); //监听系统主题变化
 
