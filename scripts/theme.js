1dc03a9008ab2a3b9c9af7fa46c9028e220c3b7a
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-10-04 22:02:40 +0800

    theme切换彻底完成!

--- scripts/theme.js
+++ scripts/theme.js
@@ -9,24 +9,28 @@ function change_theme(theme) {
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         // 如果当前按钮对应的主题与传入的主题相同，则为该按钮添加selected_theme类
-        if (button.id === theme + '_mode_btn') {
-            button.style.border="1px solid";
+        if (button.id === theme + "_mode_btn") {
+            button.style.border = "1px solid";
             button.style.borderColor = "var(--hover-color)";
         } else { // 否则，删除该按钮的selected_theme类
-            button.style.border="none";
+            button.style.border = "none";
         }
     }
+    const now_theme_img = document.getElementById("now_theme_img");
     //设置主题
     options["theme"] = theme;
     switch (theme) {
         case "light":
             document.body.classList.remove("dark_mode");
+            now_theme_img.src = "src/images/sun.svg";
             break;
         case "dark":
-            document.body.classList.add("dark_mode");
+            document.body.classList.add("dark_mode")
+            now_theme_img.src = "src/images/moon.svg";
             break;
         case "auto":
             auto_set_theme();
+            now_theme_img.src = "src/images/auto_theme.svg";
             break;
     }
     change_theme_change_dropdown(false);
