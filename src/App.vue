4bc09b18243df03565ab1720f3c29c8104511384
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-11-10 22:52:38 +0800

    水

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+const b = ref(true);
+
+function toggleB() {
+  b.value = !b.value;
+}
+</script>
 <template>
   <div id="app">
     <button @click="count++">{{ count }}</button>
-    <button @click="b = !b;">change b</button>
+    <button @click="toggleB">change b</button>
     <p>b : {{ b }}</p>
+    <p v-if="b">Can you see me? b is {{ b }}</p>
   </div>
 </template>
 
@@ -11,12 +22,8 @@
 
 export default {
   name: "App",
-  data() {
-    return {
-      count: 0,
-      b: false,
-    };
-  },
+
+
 };
 </script>
 
