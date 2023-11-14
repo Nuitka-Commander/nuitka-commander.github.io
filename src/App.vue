8d2d15f8f661a4d7bdb7286d8a5ac4c50587ba32
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-11-14 21:53:42 +0800

    to vite finish

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <div id="app">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
