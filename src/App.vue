4b5b80d78df7679007c4957d073d8470a25c2b65
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-11-14 21:33:44 +0800

    to vite

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,30 @@
 <script setup>
-import {ref} from "vue";
-
-const count = ref(0);
-const b = ref(true);
-
-function toggleB() {
-  b.value = !b.value;
-}
+import HelloWorld from './components/HelloWorld.vue'
 </script>
+
 <template>
-  <div id="app">
-    <button @click="count++">{{ count }}</button>
-    <button @click="toggleB">change b</button>
-    <p>b : {{ b }}</p>
-    <p v-if="b">Can you see me? b is {{ b }}</p>
-
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
-
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-
-
-export default {
-  name: "App",
-
-
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
