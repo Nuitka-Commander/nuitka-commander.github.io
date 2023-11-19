12db9c52191c6fc2fb5f5b1bb09bcebf3639aa6d
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-11-19 23:28:45 +0800

    try try

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": resolve(__dirname, "src"),
-            "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
         },
     }
     ,
