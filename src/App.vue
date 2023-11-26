64068bcf03e2292cb1fa8331937299c505bb4fe6
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-11-26 22:13:06 +0800

    准备开改弹出框 bkup?

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,30 @@ import constants from "@/stores/constants.json";
 
   <footer>
     <div id="footer_left">
+      <!--链接信息-->
       <div>
         <ul>
           <li><a href="https://nuitka.net/">{{ $t("website_info.nuitka_website") }}</a></li>
-          <li>项目主页Td</li>
-          <li>github网站td</li>
-          <li>gitee镜像站td</li>
-
+          <li><a href="https://github.com/nuitka-commander/nuitka-commander.github.io/">
+            {{ $t("website_info.project_homepage") }}
+          </a></li>
+          <li><a href="https://nuitka-commander.github.io/">{{ $t("website_info.github_website") }}</a></li>
+          <!--todo gitee-->
+          <li><a href="">{{ $t("website_info.gitee_website")}}</a></li>
         </ul>
       </div>
+      <!--项目开发/使用相关-->
       <div>
         <ul>
-          <li><a :href="constants.urls.report_issue">{{ $t("website_info.report_issue") }}</a></li>
-          <li><a :href="constants.urls.get_involved">{{ $t("website_info.get_involved") }}</a></li>
-          <li>更新日志td</li>
-          <li>本地使用td</li>
+          <li><a :href="constants.base_url + 'issues'">{{ $t("website_info.report_issue") }}</a></li>
+          <li><a :href="constants.base_url + 'docs/get_involved.md'">{{ $t("website_info.get_involved") }}</a></li>
+          <li><a :href="constants.base_url + 'releases/latest'">{{ $t("website_info.changelog") }}</a></li>
+          <!--todo-->
+          <li><a href="">{{$t("website_info.local_use")}}</a></li>
         </ul>
       </div>
     </div>
+    <!--版本号等-->
     <div id="footer_right">
       <ul>
         <li>
@@ -44,8 +50,8 @@ import constants from "@/stores/constants.json";
           {{ $t("website_info.supported_to_nuitka_version") }}
           <strong>{{ constants.support_nuitka_version }}</strong>
         </li>
-        <li>该版本制作人员td</li>
-        <li>开源协议td</li>
+        <li>{{ $t("website_info.current_version_author")}}</li>
+        <li>{{ $t("website_info.open_source_license")}}</li>
       </ul>
     </div>
   </footer>
--- src/assets/languages/translations/zh_cn.js
+++ src/assets/languages/translations/zh_cn.js
@@ -11,6 +11,13 @@ export default {
         report_issue: "报告问题",
         nuitka_website: "Nuitka官网",
         get_involved: "参与贡献",
+        changelog: "更新日志",
+        project_homepage: "项目主页",
+        github_website: "在Github源使用",
+        gitee_website: "在Gitee源使用",
+        local_use:"本地使用",
+        current_version_author: "当前版本贡献者",
+        open_source_license: "开源协议",
     },
 
 
