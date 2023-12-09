8511b7700b71409b1e7395fdf515b49035f24e86
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-12-09 18:55:17 +0800

    fs

--- .github/workflows/scripts/check.js
+++ .github/workflows/scripts/check.js
@@ -1,26 +1,23 @@
-import fs from "fs";
-import path, {dirname} from "path";
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-import {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const constantsPath = path.join(__dirname, "/src/stores/constants.json");
-const constants = JSON.parse(fs.readFileSync(constantsPath, "utf8"));
+const constantsPath = join(__dirname, '/src/stores/constants.json');
+const constants = JSON.parse(fs.readFileSync(constantsPath, 'utf8'));
 const version = constants.version;
 
-// 读取latest_change_log.md文件的第一行
-const changeLogPath = path.join(__dirname, "latest_change_log.md");
-const changeLogFirstLine = fs.readFileSync(changeLogPath, "utf8").split("\n")[0];
+const changeLogPath = join(__dirname, 'latest_change_log.md');
+const changeLogFirstLine = fs.readFileSync(changeLogPath, 'utf8').split('\n')[0];
 const trimmedChangeLogFirstLine = changeLogFirstLine.substring(1).trim();
 
-// 比较version和trimmedChangeLogFirstLine是否相同
 if (version !== trimmedChangeLogFirstLine) {
-    console.warn("警告: 版本号与更新日志版本号不一致，请检查是否其中一个有误\n" +
-        `读取到的版本号: ${version}\n 更新日志的版本号:${trimmedChangeLogFirstLine}`);
-    process.exit(1);//停止执行
+    console.warn(`警告: 版本号与更新日志版本号不一致，请检查是否其中一个有误
+读取到的版本号: ${version}
+更新日志的版本号:${trimmedChangeLogFirstLine}`);
+    process.exit(1);
 } else {
-    console.log("版本号与更新日志版本号一致，检查通过");
-    process.exit(0);//停止执行
+    console.log('版本号与更新日志版本号一致，检查通过');
+    process.exit(0);
 }
