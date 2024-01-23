499348ca0808d96d3119a4e54e790bf0ce8b6e6e
DESKTOP-UL157US\13568 <3135351980@qq.com>
2024-01-23 23:36:15 +0800

    将卡片封装成一个组件

--- src/components/nuitka_elements/boolOption.vue
+++ src/components/nuitka_elements/boolOption.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {user_options} from "@/vals/stores/user_options.js";
+import ElementCard from "@/components/elementCard.vue";
 
 /**
  * @Description bool选项
@@ -17,12 +18,12 @@ const content = props.content;
 </script>
 
 <template>
-  <div class="edit_content_card">
+
     <el-tooltip show-after="400">
       <template #content>
         {{ $t(`nuitka_info.${content["show"]["i18n"]}.desc`) }}
       </template>
-      <div class="edit_content_card_inside">
+      <element-card>
         <el-text size="large">{{ $t(`nuitka_info.${content["show"]["i18n"]}.name`) }}</el-text>
 
         <!--  original command-->
@@ -30,48 +31,14 @@ const content = props.content;
 
 
         <el-switch v-model="content['val']"></el-switch>
-      </div>
-
-
+      </element-card>
     </el-tooltip>
 
 
-  </div>
+
 </template>
 
 <style lang="scss" scoped>
-.edit_content_card {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  margin: 10px;
-  border: 1px solid #3b3c3d;
-  background-color: #1f1e33; //掺点怪东西
-  border-radius: 6px;
-  padding: 5px 10px;
-  min-width: 60px;
-  box-shadow: 0 0 10px #3b3c3d;
- transition: all 0.3s
- ;
-  &:hover {
-    box-shadow: 0 0 3px  #3b3c3d;
-
-  }
-
-  .edit_content_card_inside {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
 
 
-    > * {
-
-      margin: 3px;
-    }
-  }
-
-
-}
 </style>
