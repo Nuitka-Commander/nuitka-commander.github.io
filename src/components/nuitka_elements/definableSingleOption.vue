21a95397b19b71c129a843776a30457ea27c38f4
DESKTOP-UL157US\13568 <3135351980@qq.com>
2024-02-25 22:45:20 +0800

    删除singleOption card 修复一个小小的类型标注错误

--- src/components/nuitka_elements/definableSingleOption.vue
+++ src/components/nuitka_elements/definableSingleOption.vue
@@ -4,13 +4,47 @@
  * @Author: erduotong
  * @Date: 2024-02-25 19:34:22
  */
-
+import * as constants from "@/vals/constants.json";
+import ElementCard from "@/components/untils/elementCard.vue";
+/**
+ * @type {ModelRef<{
+ *  i18n: string,
+ *  val:string,
+ *  command: {
+ *    original:string,
+ *  }
+ *  enabled: boolean,
+ *  elements: {
+ *    [key: string]: {
+ *      i18n: string,
+ *      command:{
+ *        original:string,
+ *        enabled:boolean,
+ *      }
+ *    }
+ *  }
+ * }>}
+ */
+const model = defineModel();
 </script>
-
+<!--todo 完成多个可用控件-->
 <template>
+  <el-tooltip :show-after="constants.element_show_after_time" placement="top">
+    <template #content>
+      <div class="use_original_text">
+
+      </div>
+    </template>
+    <element-card>
+
+    </element-card>
+
+  </el-tooltip>
 
 </template>
 
 <style lang="scss" scoped>
-
+.use_original_text {
+  white-space: pre-wrap;
+}
 </style>
