a9c424b522e6454b7082f10718c8e424aa87f821
DESKTOP-UL157US\13568 <3135351980@qq.com>
2024-08-04 16:55:03 +0800

    ?

--- src/modules/input_parsers.js
+++ src/modules/input_parsers.js
@@ -1,8 +1,8 @@
-import {input_type} from "@/values/enums.js";
-
 /**
  * @overview 导入命令的解析器
  */
+import {input_type} from "@/values/enums.js";
+
 export const parsers = {
     [input_type.cli]: async (data) => {
         // const ast = parse(data)
