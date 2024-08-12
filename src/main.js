391bac51a95a1662915dbde0a55b494015962e98
DESKTOP-UL157US\13568 <3135351980@qq.com>
2024-08-12 23:32:05 +0800

    滚动bug fix

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import "element-plus/theme-chalk/el-loading.css";
 import "element-plus/theme-chalk/el-message.css";
 import "element-plus/theme-chalk/el-notification.css";
 import "@/assets/styles/main.scss";
-
+import "default-passive-events"
 
 init_all();
 const app = createApp(App);
