9f8b20923605b4394af2a0c850a88dea6646b64a
erduotong <3135351980@qq.com>
2024-09-06 12:52:10 +0800

    search index完工

--- src/modules/use_search.js
+++ src/modules/use_search.js
@@ -1,11 +1,52 @@
 /**
  * @fileOverview 处理一些搜索相关的逻辑
  */
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import {i18n} from "@/assets/languages/i18n.js";
 
-const basic_search_index = ref("");
 
+const basic_search_index = ref([]);
+/**
+ * 手动更新一次基础配置(方便后续computed计算，防止过多更新)
+ * @param config {Object} use_command中的original_status
+ * @return {Promise<void>}
+ */
 export const get_search_index = async (config) => {
-    console.log(config);
+    const result = [];
+    //遍历所有选项 并且生成搜索必须的参数
+    Object.keys(config).forEach(topKey => {
+        const topValue = config[topKey];
+        Object.keys(topValue).forEach(subKey => {
+            const subValue = topValue[subKey];
+            Object.keys(subValue).forEach(final_key => {
+                const final_value = subValue[final_key];
+                result.push({
+                    target_page: topKey,
+                    type: final_value.type,
+                    i18n: final_value.i18n,
+                    command: final_value.command.original,
+                });
+            });
+        });
+    });
+    basic_search_index.value = result;
 
 };
+/**
+ * 计算搜索的索引(包括i18n)
+ * @return {ref<[]>} 搜索的index
+ */
+export const search_index = computed(() => {
+    //这里因为ide的问题所以禁用检查
+    const result = basic_search_index.value.map(item => {
+        // noinspection JSUnresolvedReference
+        return {
+            target_page: item.target_page,
+            type: item.type,
+            index: `${i18n.global.t(`nuitka_info.${item.i18n}.name`)} (${item.command})`.toLowerCase(), //只提供command的搜索 desc以后再说
+        };
+    });
+    console.log("search index:", result);
+    return result;
+
+});
